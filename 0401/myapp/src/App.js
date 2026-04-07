7f85113dbe1eb7ae3cafd9bd611df97768b26e57
unknown <bigdia13@gmail.com>
2026-04-07 15:36:23 +0900

    0407

---
+++ 0401/myapp/src/App.js
@@ -0,0 +1,14 @@
+/* App.js */
+import React from 'react';
+import MyComponent from './MyComponent';
+function App() {
+return (
+<div>
+  <MyComponent name="HTML"/>
+  <MyComponent name="React"/>
+  <MyComponent name="Javascript"/>
+</div>
+
+);
+}
+export default App;
